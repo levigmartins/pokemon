c8fb1b926291696ccbbb8a060065a0f00e9886c3
Levi Martins <levi.gm99@gmail.com>
2022-02-07 10:54:06 -0300

    Client from vue3 to vue2 + vuetify install

---
+++ client/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <a href="https://cli.vuejs.org" target="_blank" rel="noopener">GAMES</a>
+    <a href="https://cli.vuejs.org" target="_blank" rel="noopener">POKEMON</a>
+    <a href="https://cli.vuejs.org" target="_blank" rel="noopener">MOVES</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.header {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+  background-color: var(--blue);
+  font-size: 15px;
+  display: flex;
+  flex-direction: row;
+}
+
+a {
+    color: white
+}
+
+</style>
--- client/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-const settings = require('../../settings.json');
-
-const api = axios.create({
-    baseURL: settings.BACKEND_URL
-})
-
-export default api;
